a2256355daeaca699cc9a457c620d687d271c67a
The Grand Dog <alex.h@me.com>
2020-08-10 09:16:36 -0400

    a simple entry level mod
    this will be used to explain the very basics of the mod loader

---
+++ mods/MyFirstCommand/src/MyFirstCommand.cs
@@ -0,0 +1,20 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+
+namespace ExampleMods {
+	// this is how the vintagestory mod loader knows how to start our mod 
+	class MyFirstCommand : ModSystem
+	{
+		// this tells the vs mod loader that this is a client side mod
+		public override bool ShouldLoad(EnumAppSide forSide) => forSide == EnumAppSide.Client;
+
+		// the vs mod loader uses this to start our mod for the client side
+		public override void StartClientSide(ICoreClientAPI api) {
+			// this creates a client side command called "hello" with the description "Says Hello!" 
+			api.RegisterCommand("hello", "Says hello!", "hello", (int groupId, CmdArgs cmdArgs) => {
+				// this says hello! :)
+				api.ShowChatMessage("Hello!");
+			});
+		}
+	}
+}
